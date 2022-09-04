5052077f345d07fc2730aaa6739771855454cb36
Rakibul Islam <newbie.rakib@gmail.com>
2022-09-04 20:20:47 +0600

    Completed authenticate page

--- src/Authenticate.java
+++ src/Authenticate.java
@@ -1,10 +1,10 @@
 import java.awt.*;
-import java.awt.geom.*;
+import java.awt.event.*;
+
 import javax.swing.*;
-import javax.swing.border.*;
 
 public class Authenticate extends JFrame{
-    JLabel label, user, pass;
+    JLabel label, user, pass, background;
     JPanel panel;
     JMenuBar mb;
     JMenu info, signUp;
@@ -15,31 +15,48 @@ public class Authenticate extends JFrame{
         super( title );                      
         setSize( 700, 600 );
 
+        
         // Input Box and Level In Panel 
         user = new JLabel("Enter Your Username");
-        user.setBounds(230, 110, 200, 50);
+        user.setBounds(230, 110, 200, 30);
         pass = new JLabel("Enter Your Password");
-        pass.setBounds(230, 165, 200, 50);
+        pass.setBounds(230, 195, 200, 30);
         add(user);
         add(pass);
         username = new JTextArea();
         password = new JPasswordField();
-        username.setBounds(230,150, 200,20);  
-        password.setBounds(230,200, 210,30);  
+        username.setBackground(Color.GRAY);
+        username.setBounds(230,150, 200,25);  
+        password.setBounds(230,225, 210,35);
+        password.setBackground(Color.GRAY);  
         add(username);
         add(password);
-
+        
         //Login Button
         login = new JButton("Login");
-        login.setBounds(230, 250, 210, 40);
+        login.setBounds(230, 260, 210, 40);
         add(login);
 
+        // Login Button Listener
+        login.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                setVisible(false);
+                new Dashboard("Dashboard");
+            }
+        });
+        
         //Login Pannel
         panel = new JPanel();
         panel.setBounds(190,100,300,250);
         panel.setBackground(Color.lightGray);
         add(panel); 
-
+        
+        // Set Background Image
+        setLayout(null);
+        ImageIcon img = new ImageIcon("img/login.png");
+        background = new JLabel("", img, JLabel.CENTER);
+        background.setBounds(0, 0, 700, 600);
+        add(background);
 
         // Menu Bar
         mb = new JMenuBar();
@@ -53,7 +70,7 @@ public class Authenticate extends JFrame{
         // Main Frame
         setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
         setLocationRelativeTo(null);  
-        setLayout( null );       
-        getContentPane().setBackground(Color.gray);
+        setLayout( null );   
+        setVisible(false);    
     }
 }
--- src/Dashboard.java
+++ src/Dashboard.java
@@ -1,3 +1,42 @@
-public class Dashboard {
-    
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Dashboard extends JFrame{
+    JLabel label, background;
+    JPanel panel;
+    JMenuBar mb;
+    JMenu info, signUp;
+    JButton login;
+
+    Dashboard(String Title){
+        super(Title);
+        setSize( 700, 600 );
+
+        login = new JButton("Login");
+        login.setBounds(230, 260, 210, 40);
+        add(login);
+
+        // Login Button Listener
+        login.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                setVisible(false);
+                Authenticate a =  new Authenticate("Dashboard");
+                a.setVisible(true);
+            }
+        });
+
+        ImageIcon img = new ImageIcon("img/Bg.png");
+        background = new JLabel("", img, JLabel.CENTER);
+        background.setBounds(0, 0, 700, 600);
+        add(background);
+
+
+        // Frame Initialization
+        setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        setLocationRelativeTo(null);  
+        setVisible(true);
+        setLayout( null );       
+        //getContentPane().setBackground(Color.MAGENTA);
+    }
 }
